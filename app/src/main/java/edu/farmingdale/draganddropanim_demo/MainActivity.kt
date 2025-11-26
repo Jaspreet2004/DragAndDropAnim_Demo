f260c173dd268155e62e8a217d36accc26f2a389
Jaspreet Singh <singhj0604@gmail.com>
2025-11-25 19:48:49 -0500

    ToDo 5: Make this works in landscape mode only

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import edu.farmingdale.draganddropanim_demo.ui.theme.DragAndDropAnim_DemoTheme
+import android.content.pm.ActivityInfo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         enableEdgeToEdge()
         setContent {
             DragAndDropAnim_DemoTheme {
